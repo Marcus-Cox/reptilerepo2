b31f8bfd053e1e34554b44b6b42eb5a5494d5e6b
Marcus-Cox <marcus.cox92@gmail.com>
2022-09-10 13:16:44 -0500

    model and fixture redos

--- reptilerepoapi/views/listing.py
+++ reptilerepoapi/views/listing.py
@@ -4,9 +4,12 @@ from turtle import title
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from reptilerepoapi.models import Listing, species
+from reptilerepoapi.models import Listing
 from reptilerepoapi.models import Lister
 from reptilerepoapi.models import Species
+from reptilerepoapi.models import Sex
+from reptilerepoapi.models import Morph
+from reptilerepoapi.models import Diet
 
 
 class ListingView(ViewSet):
@@ -32,14 +35,18 @@ class ListingView(ViewSet):
         # incoming_user = request.auth.user
         lister  = Lister.objects.get(user=request.auth.user)
         species = Species.objects.get(pk=request.data["species"])
+        sex = Sex.objects.get(pk=request.data["sex"])
+        morph = Morph.objects.get(pk=request.data["morph"])
+        diet = Diet.objects.get(pk=request.data["diet"])
+        
         
         new_listing = Listing.objects.create(
-            sex=request.data["sex"],
-            morph=request.data["morph"],
+            sex=sex,
+            morph=morph,
             age=request.data["age"],
             listing_date=request.data["listing_date"],
             hatch_date=request.data.get("hatch_date"),
-            diet=request.data.get("diet"),
+            diet=diet,
             price=request.data.get("price"),
             lister=lister,
             species=species
@@ -54,7 +61,6 @@ class ListingView(ViewSet):
         editing_listing.sex = request.data["sex"]
         editing_listing.morph = request.data["morph"]
         editing_listing.age = request.data["age"]
-        editing_listing.listing_date = request.data["listing_date"]
         editing_listing.hatch_date = request.data.get("hatch_date")
         editing_listing.diet=request.data.get("diet")
         editing_listing.price=request.data.get("price")
