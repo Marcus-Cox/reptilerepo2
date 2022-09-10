8c76c0d1b344320565ab7998a9f94a2087888276
Marcus-Cox <marcus.cox92@gmail.com>
2022-09-10 07:48:04 -0500

    model redoes

--- reptilerepoapi/models/listing.py
+++ reptilerepoapi/models/listing.py
@@ -1,16 +1,18 @@
+from operator import mod
 from tkinter import CASCADE
 from django.db import models
 from .lister import Lister
 from .species import Species
-
+from .sex import Sex
+from .morph import Morph
+from .diet import Diet
 
 class Listing(models.Model):
     lister = models.ForeignKey(Lister, on_delete=models.CASCADE)
     species = models.ForeignKey(Species, on_delete=models.CASCADE)
-    sex = models.CharField(max_length=50)
-    morph = models.CharField(max_length=50)
+    sex = models.ForeignKey(Sex, on_delete=models.CASCADE)
+    morph = models.ForeignKey(Morph, on_delete=models.CASCADE)
     age = models.IntegerField()
-    listing_date = models.DateField(auto_now_add=True)
     hatch_date = models.DateField(max_length=50)
-    diet = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    diet = models.ManyToManyField(Diet)
+    
